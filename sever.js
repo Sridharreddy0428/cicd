17a443766317c242f5f81e45099be5b33b2bd654
Sridharreddy0428 <121177682+Sridharreddy0428@users.noreply.github.com>
2023-07-26 13:33:56 +0530

    Update sever.js
    port number updated

--- sever.js
+++ sever.js
@@ -3,5 +3,5 @@ const server = http.createServer((req, res)=>{
     console.log("New Connection")
     res.end("Hello Abhi Reddy WELCOME TO CI-CD")
 })
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8001
 server.listen(PORT,()=>console.log('Listening'))
